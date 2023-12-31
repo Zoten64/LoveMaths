dc84f1100f9b9d186f7b7c3c2fc49016f970ec2a
Zoten64 <forjavligratta@gmail.com>
2023-12-31 21:43:36 +0100

    Feat: Add more javascript to listen to buttons. Make them display an alert as a placeholder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and listen for input
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button")
+
+    console.log("Script loaded")
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit");
+            }
+
+            else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+
+    }
+})
+
 function runGame(){
 
 }
